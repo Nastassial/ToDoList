f8b5415231a2f012031922b88db4094ade418ed8
Nastassial <nastassialmezhen@gmail.com>
2023-10-26 20:00:16 +0300

    Added JsonConstructor

--- MyToDoList/Data/Task.cs
+++ MyToDoList/Data/Task.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Text.Json.Serialization;
+
 namespace MyToDoList.Data;
 
 public class Task
@@ -8,10 +10,14 @@ public class Task
 
     public DateTime? CompletedTime { get; private set; }
 
-    public Task(string description)
+    public Task(string description) : this(description, DateTime.Now, null) { }
+
+    [JsonConstructor]
+    public Task(string description, DateTime createdTime, DateTime? completedTime)
     {
         Description = description;
-        CreatedTime = DateTime.Now;
+        CreatedTime = createdTime;
+        CompletedTime = completedTime;
     }
 
     public void Complete()
